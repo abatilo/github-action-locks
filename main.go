93cc1d1a1cc600649fb337681d9565acd89f57ec
Aaron <AaronBatilo@gmail.com>
2020-06-07 15:51:57 -0600

    Unifying documentation

--- main.go
+++ main.go
@@ -15,29 +15,30 @@ import (
 )
 
 const (
+	// LockTimeoutVar is the key for the setting to control how long to wait to acquire a lock, in minutes
 	LockTimeoutVar = "timeout"
-	LockTableVar   = "table"
+
+	// LockTableVar is the key for the setting to control the DynamoDB table to write the lock in
+	LockTableVar = "table"
+
+	// LockKeyNameVar is the key for the setting to control the name of the column where we write locks
 	LockKeyNameVar = "key"
-	LockNameVar    = "name"
 
+	// LockNameVar is the key for the setting to control the name of the lock
+	LockNameVar = "name"
+
+	// DefaultLockTimeout is the default time, in minutes, for how long to wait to acquire a lock before giving up
 	DefaultLockTimeout = 30
-	DefaultLockTable   = "github-action-locks"
-	DefaultLockKeyName = "LockID"
-	DefaultLockName    = "foobar"
-)
 
-func createLock(ctx context.Context, svc *dynamodb.DynamoDB, lockTable, lockKeyName, lockName string) error {
-	_, err := svc.PutItem(&dynamodb.PutItemInput{
-		TableName: aws.String(lockTable),
-		Item: map[string]*dynamodb.AttributeValue{
-			lockKeyName: {
-				S: aws.String(lockName),
-			},
-		},
-		ConditionExpression: aws.String(fmt.Sprintf("attribute_not_exists(%s)", lockKeyName)),
-	})
-	return err
-}
+	// DefaultLockTable is the default name of the DynamoDB table to write the lock in
+	DefaultLockTable = "github-action-locks"
+
+	// DefaultLockKeyName is the default name of the column where we write locks
+	DefaultLockKeyName = "LockID"
+
+	// DefaultLockName is the default name for the lock to create
+	DefaultLockName = "foobar"
+)
 
 func lock() *cobra.Command {
 	cmd := &cobra.Command{
@@ -49,6 +50,7 @@ func lock() *cobra.Command {
 			LockKeyName := viper.GetString(LockKeyNameVar)
 			LockName := viper.GetString(LockNameVar)
 
+			log.Print("Creating lock with the following parameters:")
 			log.Printf("LockTimeout: %v", LockTimeout)
 			log.Printf("LockTable: %v", LockTable)
 			log.Printf("LockKeyName: %v", LockKeyName)
@@ -58,11 +60,17 @@ func lock() *cobra.Command {
 			ctx, cancel := context.WithTimeout(context.Background(), time.Duration(LockTimeout)*time.Minute)
 			defer cancel()
 
-			// AcquireLock
 			log.Println("Acquiring lock")
-
 			for {
-				err := createLock(ctx, svc, LockTable, LockKeyName, LockName)
+				_, err := svc.PutItem(&dynamodb.PutItemInput{
+					TableName: aws.String(LockTable),
+					Item: map[string]*dynamodb.AttributeValue{
+						LockKeyName: {
+							S: aws.String(LockName),
+						},
+					},
+					ConditionExpression: aws.String(fmt.Sprintf("attribute_not_exists(%s)", LockKeyName)),
+				})
 
 				if err != nil {
 					if aerr, ok := err.(awserr.Error); ok {
@@ -85,16 +93,16 @@ func lock() *cobra.Command {
 		},
 	}
 
-	cmd.PersistentFlags().Int(LockTimeoutVar, DefaultLockTimeout, "How long to wait and retry acquiring a lock if it's already been acquired. Value in minutes")
+	cmd.PersistentFlags().Int(LockTimeoutVar, DefaultLockTimeout, "How long to wait to acquire a lock, in minutes")
 	viper.BindPFlag(LockTimeoutVar, cmd.PersistentFlags().Lookup(LockTimeoutVar))
 
-	cmd.PersistentFlags().String(LockTableVar, DefaultLockTable, "The name of the DynamoDB table to create the lock in")
+	cmd.PersistentFlags().String(LockTableVar, DefaultLockTable, "DynamoDB table to write the lock in")
 	viper.BindPFlag(LockTableVar, cmd.PersistentFlags().Lookup(LockTableVar))
 
-	cmd.PersistentFlags().String(LockKeyNameVar, DefaultLockKeyName, "The name of the column in the DynamoDB table to use for storing the lock name")
+	cmd.PersistentFlags().String(LockKeyNameVar, DefaultLockKeyName, "Name of the column where we write locks")
 	viper.BindPFlag(LockKeyNameVar, cmd.PersistentFlags().Lookup(LockKeyNameVar))
 
-	cmd.PersistentFlags().String(LockNameVar, DefaultLockName, "The name of the DynamoDB table to create the lock in")
+	cmd.PersistentFlags().String(LockNameVar, DefaultLockName, "Name of the lock")
 	viper.BindPFlag(LockNameVar, cmd.PersistentFlags().Lookup(LockNameVar))
 
 	return cmd
@@ -105,17 +113,14 @@ func unlock() *cobra.Command {
 		Use:   "unlock",
 		Short: "Release a lock",
 		Run: func(_ *cobra.Command, _ []string) {
-			LockTimeout := viper.GetInt(LockTimeoutVar)
 			LockTable := viper.GetString(LockTableVar)
 			LockKeyName := viper.GetString(LockKeyNameVar)
 			LockName := viper.GetString(LockNameVar)
 
 			svc := dynamodb.New(session.Must(session.NewSession()))
-			ctx, cancel := context.WithTimeout(context.Background(), time.Duration(LockTimeout)*time.Minute)
-			defer cancel()
 
 			log.Println("Acquiring lock to release it")
-			output, err := svc.GetItemWithContext(ctx, &dynamodb.GetItemInput{
+			output, err := svc.GetItem(&dynamodb.GetItemInput{
 				TableName:      aws.String(LockTable),
 				ConsistentRead: aws.Bool(true),
 				Key: map[string]*dynamodb.AttributeValue{
@@ -145,34 +150,28 @@ func unlock() *cobra.Command {
 		},
 	}
 
-	cmd.PersistentFlags().Int(LockTimeoutVar, DefaultLockTimeout, "How long to wait and retry acquiring a lock if it's already been acquired. Value in minutes")
-	viper.BindPFlag(LockTimeoutVar, cmd.PersistentFlags().Lookup(LockTimeoutVar))
-
-	cmd.PersistentFlags().String(LockTableVar, DefaultLockTable, "The name of the DynamoDB table to create the lock in")
+	cmd.PersistentFlags().String(LockTableVar, DefaultLockTable, "DynamoDB table to delete the lock from")
 	viper.BindPFlag(LockTableVar, cmd.PersistentFlags().Lookup(LockTableVar))
 
-	cmd.PersistentFlags().String(LockKeyNameVar, DefaultLockKeyName, "The name of the column in the DynamoDB table to use for storing the lock name")
+	cmd.PersistentFlags().String(LockKeyNameVar, DefaultLockKeyName, "Name of the column where we write locks")
 	viper.BindPFlag(LockKeyNameVar, cmd.PersistentFlags().Lookup(LockKeyNameVar))
 
-	cmd.PersistentFlags().String(LockNameVar, DefaultLockName, "The name of the DynamoDB table to create the lock in")
+	cmd.PersistentFlags().String(LockNameVar, DefaultLockName, "Name of the lock")
 	viper.BindPFlag(LockNameVar, cmd.PersistentFlags().Lookup(LockNameVar))
+
 	return cmd
 }
 
 func main() {
-
-	var (
-		rootCmd = &cobra.Command{
-			Use:   "github-action-locks",
-			Short: "Create a distributed lock for a GitHub Action",
-		}
-	)
+	rootCmd := &cobra.Command{
+		Use:   "github-action-locks",
+		Short: "Create a distributed lock for a GitHub Action",
+	}
 
 	viper.SetEnvPrefix("INPUT")
 	viper.AutomaticEnv()
 
 	rootCmd.AddCommand(lock())
 	rootCmd.AddCommand(unlock())
-
 	rootCmd.Execute()
 }
